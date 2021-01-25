3342b7503942b36204818aa4ddcf895ea3473ccf
P.A <65981103+PA-QC@users.noreply.github.com>
2021-01-25 18:52:47 -0500

    Create CGI.py

---
+++ CGI.py
@@ -0,0 +1,4 @@
+import webbrowser
+
+chrome = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
+webbrowser.open("https://drive.google.com/drive/folders/1M8vQ-aEX9LtWZqwVi1r8jEVH0T1RMjJK?usp=sharing")
