95bda531d59dee901e35c2397526b7b7dceed456
P.A <65981103+PA-QC@users.noreply.github.com>
2021-01-25 20:04:18 -0500

    Update CGI.py

--- CGI.py
+++ CGI.py
@@ -1,4 +1,25 @@
 import webbrowser
-
-chrome = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
-webbrowser.open("https://drive.google.com/drive/folders/1M8vQ-aEX9LtWZqwVi1r8jEVH0T1RMjJK?usp=sharing")
+print("/-----------------------------------------/Welcome/---------------------------------------------/")
+print("\n\nOpen Readme.txt File to connect")
+username = "user"
+password = "d666"
+d66 = "password"
+user = "username"
+print("\nPlease connect")
+username = input("Enter Username:")
+if not username =="user":
+    exit("Authentification Error")
+if username ==("user"):
+    print("Succes")
+password = input("\nEnter the password:")
+if not password =="d666":
+    exit("Authentification Error")
+if password =="d666":
+    print("Succes")
+print("/-----------------------------------------/Choose/---------------------------------------------/")
+Choose = "Choose1"
+print("\n\n1- Cracked games\n2- Next Update\n3- Next Update\n4- Next Update\n5- Next Update")
+input("Choose")
+if Choose =="1":
+    chrome = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
+webbrowser.open("https://drive.google.com/drive/folders/1M8vQ-aEX9LtWZqwVi1r8jEVH0T1RMjJK?usp=sharing%22")
