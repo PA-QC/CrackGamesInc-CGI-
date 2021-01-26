a98a8dcc320f6233c256d4e86a76353a7b88101e
P.A <65981103+PA-QC@users.noreply.github.com>
2021-01-25 20:55:56 -0500

    Update CGI.py

--- CGI.py
+++ CGI.py
@@ -1,25 +1,50 @@
+# Imports
 import webbrowser
-print("/-----------------------------------------/Welcome/---------------------------------------------/")
-print("\n\nOpen Readme.txt File to connect")
-username = "user"
-password = "d666"
-d66 = "password"
-user = "username"
-print("\nPlease connect")
-username = input("Enter Username:")
-if not username =="user":
-    exit("Authentification Error")
-if username ==("user"):
-    print("Succes")
-password = input("\nEnter the password:")
-if not password =="d666":
-    exit("Authentification Error")
+import platform
+from colorama import Fore
+
+# Current OS
+os = platform.system()
+
+# Default Login
+user = "user"
+passwd = "d666"
+
+# Exit Codes
+
+# Auth Error
+# AUTH-01 : Bad credentials (Username)
+# AUTH-02 : Bad credentials (Password)
+
+# Text Color
+print(Fore.MAGENTA)
+
+# Starting
+print("-=-=-=-=-=-=-=-=-=-=-=-=-=-= Welcome =-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+print(" ")
+print("Your current Operating System is: " + os)
+
+print("\nOpen Readme.txt file, the creds are here")
+print("\nPlease sign in")
+
+# Username verification
+username = input("Enter Username: ")
+if username != user:
+    exit("✗ | Authentication Error (Exit Code : AUTH-01)")
+elif username == user:
+    print("\n✓ | Success")
+
+# Password verification
+password = input("\nEnter your password: ")
+if password != passwd:
+    exit("\n✗ | Authentication Error (Exit Code: AUTH-02)")
 if password =="d666":
-    print("Succes")
-print("/-----------------------------------------/Choose/---------------------------------------------/")
-Choose = "Choose1"
-print("\n\n1- Cracked games\n2- Next Update\n3- Next Update\n4- Next Update\n5- Next Update")
-input("Choose")
-if Choose =="1":
-    chrome = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
-webbrowser.open("https://drive.google.com/drive/folders/1M8vQ-aEX9LtWZqwVi1r8jEVH0T1RMjJK?usp=sharing%22")
+    print("\n✓ | Success")
+
+# Options
+option = "1"
+print("-=-=-=-=-=-=-=-=-=-=-=-=-=-= Options =-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+print("\n\n1- Cracked games\n2- Soon...\n3- Soon...\n4- Soon...\n5- Soon...")
+input("\nEnter your Choice")
+if option =="1":
+    webbrowser.open("https://drive.google.com/drive/folders/1M8vQ-aEX9LtWZqwVi1r8jEVH0T1RMjJK?usp=sharing%22")
